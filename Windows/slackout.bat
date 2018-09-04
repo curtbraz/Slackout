0d1b432a4df2d47a5a3ef449d0adc45e07cb846a
Curtis Brazzell <curtbraz@gmail.com>
2018-09-04 02:49:17 -0400

    Update slackout.bat

--- Windows/slackout.bat
+++ Windows/slackout.bat
@@ -16,5 +16,8 @@ IF "%~1" == "" (set channel=%Slack_Channel%)
 
 IF NOT "%~1" == "" (set channel=%1)
 
+:: Echoes to CMD Prompt
+echo !stdin!
+
 :: Makes Curl Request to API
 curl -s -F "content=!stdin!" -F "channels=%channel%" -F "title=Slack Stdout" -H "Authorization: Bearer %Slack_API%" https://slack.com/api/files.upload
