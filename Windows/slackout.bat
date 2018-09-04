a4ce756f3e7488fdf28f356d7db80b2a2f8e83d2
Curtis Brazzell <curtbraz@gmail.com>
2018-09-04 02:18:47 -0400

    Add files via upload

---
+++ Windows/slackout.bat
@@ -0,0 +1,20 @@
+:: Insert Default Slack Channel & API Token Here
+@echo off
+set Slack_Channel="#YOUR_SLACK_CHANNEL_HERE"
+set Slack_API="YOUR_LEGACY_SLACK_TOKEN_HERE"
+
+setlocal enabledelayedexpansion
+
+:: Reads stdin and Passes to Slack API in Text Snippet
+for /F "tokens=*" %%a in ('more') do (
+  set stdin=!stdin!^
+
+%%a
+)
+
+IF "%~1" == "" (set channel=%Slack_Channel%)
+
+IF NOT "%~1" == "" (set channel=%1)
+
+:: Makes Curl Request to API
+curl -s -F "content=!stdin!" -F "channels=%channel%" -F "title=Slack Stdout" -H "Authorization: Bearer %Slack_API%" https://slack.com/api/files.upload
